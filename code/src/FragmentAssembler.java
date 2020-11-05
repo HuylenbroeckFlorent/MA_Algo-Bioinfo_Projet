0f5bba8d82ef3f736660be0960133d44b09a9816
HuylenbroeckFlorent <huylenbroeck.florent@gmail.com>
2020-11-05 14:46:10 +0100

    first commit

--- code/src/FragmentAssembler.java
+++ code/src/FragmentAssembler.java
@@ -1,6 +1,41 @@
+import java.io.*;
+import java.util.*;
+
 class FragmentAssembler{
 
+	public static ArrayList<String> fragments;
+
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		fragments = OpenFasta(args[0]);
+	}
+
+
+	/**
+	* Opens a .fasta file and imports the fragments that it describes.
+	*/
+	public static ArrayList<String> OpenFasta(String path){
+		ArrayList<String> fragments = new ArrayList<String>();
+		try{
+			BufferedReader fastareader = new BufferedReader(new FileReader(path));
+			String line;
+			String fragment = "";
+			while((line = fastareader.readLine()) != null){
+				if (line.charAt(0)=='>'){
+					if (fragment!=""){
+						fragments.add(fragment);
+						fragment="";
+					}
+				}
+				else{
+					fragment+=line;
+				}
+			}
+			if (fragment!="")
+				fragments.add(fragment);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return fragments;
 	}
 }
