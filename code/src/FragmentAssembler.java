da7ca52b61e9f2d80ce12a66c0ba4c5ef826ea7b
HuylenbroeckFlorent <huylenbroeck.florent@gmail.com>
2020-11-05 16:03:32 +0100

    naive occurence finder

--- code/src/FragmentAssembler.java
+++ code/src/FragmentAssembler.java
@@ -3,10 +3,14 @@ import java.util.*;
 
 class FragmentAssembler{
 
+	/**
+	* ArrayList that holds the fragments.
+	*/
 	public static ArrayList<String> fragments;
 
 	public static void main(String[] args){
 		fragments = OpenFasta(args[0]);
+
 	}
 
 
@@ -37,5 +41,53 @@ class FragmentAssembler{
 		}
 
 		return fragments;
+	}
+
+	/**
+	* Finds every occurences of a word in a sequence (the naive way).
+	*/
+	public static int[] FindOccurencesNaive(String seq, String word){
+		int comp = 0;
+		if(seq.length()<word.length())
+			return null;
+		for(int i=0; i<seq.length()-(word.length()+1); i++){
+			int j=0;
+			while(j<word.length()-1){
+				if(word.charAt(j)==seq.charAt(i+j)){
+					j++;
+					comp++;
+				}
+				else
+					break;
+			}
+			if(j==word.length()-1)
+				System.out.println(i);
+		}
+		System.out.println(comp);
+		return null;
+	}
+
+	/**
+	* Finds every occurences of a word in a sequence (Knuth, Morris, Pratt).
+	*/
+	public static int[] FindOccurencesKMP(String seq, String word){
+		int comp = 0;
+		if(seq.length()<word.length())
+			return null;
+		for(int i=0; i<seq.length()-(word.length()+1); i++){
+			int j=0;
+			while(j<word.length()-1){
+				if(word.charAt(j)==seq.charAt(i+j)){
+					j++;
+					comp++;
+				}
+				else
+					break;
+			}
+			if(j==word.length()-1)
+				System.out.println(i);
+		}
+		System.out.println(comp);
+		return null;
 	}
 }
