c447ca71daa3b4f4f78994b0f8145012a9df37f4
HuylenbroeckFlorent <huylenbroeck.florent@gmail.com>
2020-11-10 18:35:01 +0100

    semiglobal alignment started

--- code/src/FragmentAssembler.java
+++ code/src/FragmentAssembler.java
@@ -6,6 +6,9 @@ class FragmentAssembler{
 	/* NOTES
 	* DOTMATCHER INVERSE LES AXES ???
 	*
+	* A><T
+	* C><G
+	*
 	* (byte) '-' == 45
 	* (byte) 'a' == 97
 	* (byte) 'c' == 99
@@ -21,6 +24,8 @@ class FragmentAssembler{
 	/***/
 	public static void main(String[] args){;
 		fragments = OpenFasta(args[0]);
+
+		SemiGlobalAlignment(new byte[]{(byte) 'a', (byte) 'a',(byte) 'a',(byte) 'c'},  new byte[]{(byte) 'a',(byte) 'g',(byte) 'c'});
 	}
 
 
@@ -54,5 +59,42 @@ class FragmentAssembler{
 		}
 
 		return fragments;
+	}
+
+
+	/**
+	* Computes the semi global alignment of two fragments.
+	*
+	*
+	*
+	*/
+	private static int[][] SemiGlobalAlignment(byte[] fragment1, byte[] fragment2){
+		int m = fragment1.length+1, n = fragment2.length+1, gap_score = -2, mismatch_score = -1, match_score = 1;
+
+		int[][] sims = new int[m+1][n+1];
+
+		for(int i = 0; i<m; i++){
+			sims[i][0] = 0; // Favors gaps in the beginning of the sequence, was i*gap_score;
+		}
+
+		for(int j = 0; j<n; j++){
+			sims[0][j] = 0; // Favors gaps in the beginning of the sequence, was j*gap_score;
+		}
+
+		for(int i = 1; i<m; i++){
+			for(int j = 1; j<n; j++){
+				int p = (fragment1[i-1] == fragment2[j-1]) ? 1:-1; // -1 to shift the entries in the sims tab
+				sims[i][j] = Math.max(sims[i-1][j] + gap_score, Math.max(sims[i-1][j-1] + p, sims[i][j-1] + gap_score));
+			}
+		}
+
+		for(int i = 1; i<m; i++){
+			for(int j = 1; j<n; j++){
+				System.out.print(sims[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+		return sims;
 	}
 }
