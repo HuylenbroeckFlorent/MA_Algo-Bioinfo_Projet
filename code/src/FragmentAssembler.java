7da1b5ecf52b75079c46196c98002647ed55695d
HuylenbroeckFlorent <huylenbroeck.florent@gmail.com>
2020-11-10 15:05:45 +0100

    .fasta opning done

--- code/src/FragmentAssembler.java
+++ code/src/FragmentAssembler.java
@@ -3,30 +3,43 @@ import java.util.*;
 
 class FragmentAssembler{
 
+	/* NOTES
+	* DOTMATCHER INVERSE LES AXES ???
+	*
+	* (byte) '-' == 45
+	* (byte) 'a' == 97
+	* (byte) 'c' == 99
+	* (byte) 'g' == 103
+	* (byte) 't' == 116
+	*/
+
 	/**
 	* ArrayList that holds the fragments.
 	*/
-	public static ArrayList<String> fragments;
+	public static ArrayList<byte[]> fragments;
 
-	public static void main(String[] args){
+	/***/
+	public static void main(String[] args){;
 		fragments = OpenFasta(args[0]);
-
 	}
 
 
 	/**
 	* Opens a .fasta file and imports the fragments that it describes.
+	*
+	* @param path 	String, path to the .fasta file.
+	* @return 		Arraylist<byte[]> that contains all the retreived fragments, stored as individuals byte arrays (one byte = one char) in an Arraylist.
 	*/
-	public static ArrayList<String> OpenFasta(String path){
-		ArrayList<String> fragments = new ArrayList<String>();
+	private static ArrayList<byte[]> OpenFasta(String path){
+		ArrayList<byte[]> fragments = new ArrayList<byte[]>();
 		try{
-			BufferedReader fastareader = new BufferedReader(new FileReader(path));
+			BufferedReader fastaReader = new BufferedReader(new FileReader(path));
 			String line;
 			String fragment = "";
-			while((line = fastareader.readLine()) != null){
+			while((line = fastaReader.readLine()) != null){
 				if (line.charAt(0)=='>'){
 					if (fragment!=""){
-						fragments.add(fragment);
+						fragments.add(fragment.getBytes());
 						fragment="";
 					}
 				}
@@ -35,35 +48,11 @@ class FragmentAssembler{
 				}
 			}
 			if (fragment!="")
-				fragments.add(fragment);
+				fragments.add(fragment.getBytes());
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 
 		return fragments;
-	}
-
-	/**
-	* Finds every occurences of a word in a sequence (the naive way).
-	*/
-	public static int[] FindOccurencesNaive(String seq, String word){
-		int comp = 0;
-		if(seq.length()<word.length())
-			return null;
-		for(int i=0; i<seq.length()-(word.length()+1); i++){
-			int j=0;
-			while(j<word.length()-1){
-				if(word.charAt(j)==seq.charAt(i+j)){
-					j++;
-					comp++;
-				}
-				else
-					break;
-			}
-			if(j==word.length()-1)
-				System.out.println(i);
-		}
-		System.out.println(comp);
-		return null;
 	}
 }
